6f62734f9e26276a375e5bbf889ceba6ed218e96
Matheus Viana <mtviana42@gmail.com>
2024-02-28 20:02:36 -0300

    feat: setup database connection

---
+++ src/database/database.ts
@@ -0,0 +1,46 @@
+import sqlite3 from 'sqlite3';
+import { open, Database } from 'sqlite';
+
+sqlite3.verbose()
+
+let database: Database | null = null;
+
+export async function setupDatabase(): Promise<Database> {
+  try {
+    if (!database) {
+      database = await open({
+        filename: './src/database/url-shortener.db',
+        driver: sqlite3.Database,
+      });
+
+      await database.exec(`
+        DROP TABLE urls;
+        CREATE TABLE urls (
+          id TEXT PRIMARY KEY,
+          original_url TEXT NOT NULL,
+          short_url TEXT NOT NULL,
+          short_url_key TEXT NOT NULL UNIQUE,
+          total_access INTEGER DEFAULT 0
+        );
+      `);
+
+      console.log('Database setup successful.');
+    }
+
+    if (!database) {
+      throw new Error('Database instance is null after setup.');
+    }
+
+    return database;
+  } catch (error) {
+    console.error('Error setting up database:', error);
+    throw error;
+  }
+}
+
+export function getDatabase(): Database {
+  if (!database) {
+    throw new Error('Database instance is not initialized.');
+  }
+  return database;
+}
